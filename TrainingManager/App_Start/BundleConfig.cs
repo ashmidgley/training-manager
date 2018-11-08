84ef6803ead3a5e16d8a319d899d3c37def8250c
AshleyM <AshleyM@WKS-1886>
2018-11-09 11:46:41 +1300

    Update to latest stable version of jQuery. Refactor of jQueryValidate into jQuery bundle and removal of unnecessary jqueryval bundle references.

--- TrainingManager/App_Start/BundleConfig.cs
+++ TrainingManager/App_Start/BundleConfig.cs
@@ -24,10 +24,7 @@ namespace TrainingManager
             ));
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"
-            ));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                "~/Scripts/jquery-{version}.js",
                 "~/Scripts/jquery.validate*"
             ));
 
