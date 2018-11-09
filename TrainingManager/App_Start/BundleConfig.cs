2b41f201ea46c1cf37b97af33d5b8fc2d9e89629
AshleyM <AshleyM@WKS-1886>
2018-11-09 15:22:55 +1300

    Initial update to bootstrap 4.

--- TrainingManager/App_Start/BundleConfig.cs
+++ TrainingManager/App_Start/BundleConfig.cs
@@ -35,13 +35,14 @@ namespace TrainingManager
             ));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js",
+                "~/Scripts/umd/popper.min.js",
+                "~/Scripts/bootstrap.min.js",
                 "~/Scripts/respond.js",
                 "~/Scripts/bootbox.min.js"
             ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.css",
+                "~/Content/bootstrap.min.css",
                 "~/Content/star-rating.min.css",
                 "~/Content/site.css"
             ));
