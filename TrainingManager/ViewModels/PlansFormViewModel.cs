f730aaf8c98837668164325c4c3fe883d919547d
AshleyM <AshleyM@WKS-1886>
2018-11-14 12:33:28 +1300

    Setting smaller max values for plan name, workout name and user email. Fixing bug in UI caused by 2 line plan name.

--- TrainingManager/ViewModels/PlansFormViewModel.cs
+++ TrainingManager/ViewModels/PlansFormViewModel.cs
@@ -12,6 +12,7 @@ namespace TrainingManager.ViewModels
     {
         public int Id { get; set; }
         [Required]
+        [StringLength(50)]
         public string Name { get; set; }
         [Required]
         public int PlanId { get; set; }
